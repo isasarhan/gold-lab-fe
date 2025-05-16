158ec95d02168364ed8abd4e4feb092347ddda01
issa98 <isasarhan.lz@gmail.com>
2025-05-16 16:55:39 +0300

    resolving build issues

---
+++ src/app/admin/employees/attendence/add/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+export interface AddAttendencePageProps {}
+
+const AddAttendencePage: FC<AddAttendencePageProps> = () => {
+  return (
+    <div>
+      Hello AddAttendencePage
+    </div>
+  );
+};
+
+export default AddAttendencePage;
