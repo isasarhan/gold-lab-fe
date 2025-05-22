48bbdcf9a10a0aa7f59fdbc9bcdd005ec889f8a9
issa98 <isasarhan.lz@gmail.com>
2025-05-22 09:44:51 +0300

    adding pagination in dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -4,19 +4,26 @@ import useBalances from '@/services/balances';
 import useCustomers from '@/services/customers';
 import React, { FC } from 'react';
 
-export interface DashboardPageProps { }
+export interface DashboardPageProps {
+  searchParams: Promise<{
+    query: string
+    page: number;
+  }>
+}
 
-const DashboardPage: FC<DashboardPageProps> = async () => {
+const DashboardPage: FC<DashboardPageProps> = async ({ searchParams }) => {
+  const { query, page } = await searchParams
+
   const { token } = await getAuth();
 
   const { getTotal } = useBalances({ token })
   const { getTypesAnalytics } = useCustomers({ token })
-  const { getAll:getCustomers } = useCustomers({ token })
+  const { getAll: getCustomers } = useCustomers({ token })
 
-  const [total, customersAnalytics, customers] = await Promise.all([getTotal(), getTypesAnalytics(), getCustomers({pageSize:10})]);
-  
+  const [total, customersAnalytics, customers] = await Promise.all([getTotal(), getTypesAnalytics(), getCustomers({ searchTerm: query, page, pageSize: 10 })]);
+
   return (
-    <AdminDashboardModule balanceTotal={total} customersAnalytics={customersAnalytics} customers={customers}/>
+    <AdminDashboardModule balanceTotal={total} customersAnalytics={customersAnalytics} customers={customers} />
   );
 };
 
